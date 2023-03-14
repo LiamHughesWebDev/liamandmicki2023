67d8452e3ce4774c9a53babef44127f92b28d58c
Liam Hughes <ljhughes599@gmail.com>
2023-03-14 15:33:21 -0400

    Initializing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAeFmWYKDOYBsumlUTBVgtSzoM91amPrME",
+    authDomain: "lmweddingwebsite.firebaseapp.com",
+    projectId: "lmweddingwebsite",
+    storageBucket: "lmweddingwebsite.appspot.com",
+    messagingSenderId: "604595680871",
+    appId: "1:604595680871:web:cef156c209f128e91586c2",
+    measurementId: "G-FWEMJ8HG0G"
+  }
 };
 
 /*
